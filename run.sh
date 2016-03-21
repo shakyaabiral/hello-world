f9551fa52ca7b75f6e27c8cf6f8ec6377dbbbf94
Abiral Shakya <shakyaabiral@gmail.com>
2016-03-22 09:45:24 +1100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-ln -s ../zookal-data/var /usr/share/nginx/html/demodeploy/htdocs/var
-ln -s ../zookal-data/sitemap /usr/share/nginx/html/demodeploy/htdocs/sitemap
-ln -s ../zookal-data/media /usr/share/nginx/html/demodeploy/htdocs/media
+tar zxvf backups.tgz
+#ln -s ../zookal-data/var /usr/share/nginx/html/demodeploy/htdocs/var
+#ln -s ../zookal-data/sitemap /usr/share/nginx/html/demodeploy/htdocs/sitemap
+#ln -s ../zookal-data/media /usr/share/nginx/html/demodeploy/htdocs/media
