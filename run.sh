48f7ff15ae8c1b8f5184c59edc6d422e58bac5f6
Abiral Shakya <shakyaabiral@gmail.com>
2016-03-22 09:58:26 +1100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-tar zxvf BUILD_NUMBER.tgz
+tar zxvf /usr/share/nginx/html/demodeploy/BUILD_NUMBER.tgz -C /usr/share/nginx/html/demodeploy
 #ln -s ../zookal-data/var /usr/share/nginx/html/demodeploy/htdocs/var
 #ln -s ../zookal-data/sitemap /usr/share/nginx/html/demodeploy/htdocs/sitemap
 #ln -s ../zookal-data/media /usr/share/nginx/html/demodeploy/htdocs/media
