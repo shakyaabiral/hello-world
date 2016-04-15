86ccf5c38e45e58b38660d360732bbbf8bc3f14b
Abiral Shakya <shakyaabiral@gmail.com>
2016-04-15 11:52:57 +1000

    Delete run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-tar zxvf /usr/share/nginx/html/demodeploy/BUILD_NUMBER.tgz -C /usr/share/nginx/html/demodeploy
-#ln -s ../zookal-data/var /usr/share/nginx/html/demodeploy/htdocs/var
-#ln -s ../zookal-data/sitemap /usr/share/nginx/html/demodeploy/htdocs/sitemap
-#ln -s ../zookal-data/media /usr/share/nginx/html/demodeploy/htdocs/media
