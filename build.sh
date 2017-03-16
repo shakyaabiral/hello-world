3922b0b137c8c9c8b33a6fd49a5473acc6b95f14
vagrant <vagrant@local.puphpet>
2017-03-16 17:48:02 +1100

    test

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-git flow init -d
-new_version=`bumpversion --dry-run --list minor | grep new_version | sed -r s,"^.*=",,`
-git flow feature release ${new_version}
-echo 'run build job'
-bumpversion dev
